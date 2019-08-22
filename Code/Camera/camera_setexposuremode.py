bab4b8622cedc9b8afba73a48ede863989b1a4a5
Duo Chen <dchen@slb.com>
2019-08-22 16:24:13 -0500

    check in code

---
+++ Code/Camera/camera_setexposuremode.py
@@ -0,0 +1,10 @@
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+
+camera.start_preview()
+camera.exposure_mode = 'beach'
+sleep(5)
+camera.capture('/home/pi/Desktop/beach.jpg')
+camera.stop_preview()
