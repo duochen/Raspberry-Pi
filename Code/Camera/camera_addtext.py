ea82a390875a96e688551b577e22cd85e00ded30
Duo Chen <dchen@slb.com>
2019-08-22 15:03:20 -0500

    check in code

---
+++ Code/Camera/camera_addtext.py
@@ -0,0 +1,14 @@
+from picamera import PiCamera
+from picamera import PiCamera, Color
+from time import sleep
+
+camera = PiCamera()
+
+camera.start_preview()
+camera.annotate_background = Color('blue')
+camera.annotate_foreground = Color('yellow')
+camera.annotate_text = "Hello world!"
+camera.annotate_text_size = 50
+sleep(5)
+camera.capture('/home/pi/Desktop/text.jpg')
+camera.stop_preview()
